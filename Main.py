b0796c3ec296ada0f0f27515ca250387715b976f
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2018-05-29 16:40:00 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,9 @@ import discord
 import asyncio
 import random
 import requests
-import Config
+from Config import TOKEN as token
+from Config import VERSION as vers
+from Config import CREATORS as crea
 import json
 import strawpoll
 
