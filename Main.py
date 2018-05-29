ad0f389d0779dba6d9e50c8a84613825ea29d2ba
sailveron <39734941+sailveron@users.noreply.github.com>
2018-05-29 16:54:35 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,18 @@ from Config import liel as l
 t.speed(0)
 
 def Liel():
-  
+  def right(): 
+    l.seth(0)
+    l.forward(100)
+  def left():
+    l.seth(180)
+    l.forward(100)
+  def up():
+    l.seth(90)
+    l.forward(100)
+  def down():
+    l.seth(270)
+    t.forward(100)
   
 def Tal():
 
